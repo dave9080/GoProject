4d7aae32403402fbf9d92ee54247e408ff8829ca
dave9080 <dave9080@gmail.com>
2021-04-29 21:15:22 -0700

    added go mod to 03_packages

---
+++ 03_packages/go.mod
@@ -0,0 +1,3 @@
+module github.com/dave9080/GoProject1
+
+go 1.16
--- 03_packages/main.go
+++ 03_packages/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+
+	"github.com/dave9080/GoProject1/03_packages/strutil"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println(math.Floor(2.7))
+	fmt.Println(math.Ceil(2.7))
+	fmt.Println(math.Sqrt(16))
+	fmt.Println(strutil.Reverse("olleh"))
 }
